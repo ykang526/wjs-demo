f30632ca4df4094ffb87aa9b15649da4fd26ddeb
ykang526 <80201101+ykang526@users.noreply.github.com>
2022-06-01 15:20:35 +0900

    35min

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const express = require ('express');
 const app = express();
 
+app.use(express.json());
+
+
+
+const courses = [
+    {id: 1, name: 'course 1'},
+    {id: 2, name: 'course 2'},
+    {id: 3, name: 'course 3'},
+]
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
 app.get('/api/courses', (req, res) => {
+    res.send(courses);
+});
 
+//posting to the collection of course's'
+app.post('/api/courses', (req,res)=> {
+    const course = {
+        id: courses.length+1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+
+app.get('/api/courses/:id', (req,res) => {
+    const course = courses.find(c=>c.id===parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with the given ID was not found');
+    res.send(course);
 });
 
 const port = process.env.PORT || 3000;
