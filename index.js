c98fe648c935d94a0e4f68cd6d7c3a0ac4b93e22
ykang526 <ykang19@illinois.edu>
2022-06-01 22:43:19 +0900

    finished put. need to test with postman

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi'); // npm i joi
 const express = require ('express');
 const app = express();
 
@@ -21,6 +22,20 @@ app.get('/api/courses', (req, res) => {
 
 //posting to the collection of course's'
 app.post('/api/courses', (req,res)=> {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    const result = Joi.ValidationError(req.body, schema); // returns an object
+    console.log(result);
+
+    if (result.error) { // checking if client sent a valid input
+        //400 bad request by convention
+        //res.status(400).send('This is not a valid input'); USE JOI INSTEAD
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
     const course = {
         id: courses.length+1,
         name: req.body.name
@@ -35,6 +50,29 @@ app.get('/api/courses/:id', (req,res) => {
     if (!course) res.status(404).send('The course with the given ID was not found');
     res.send(course);
 });
+
+app.put('/api/courses/:id', (req, res) => {
+    //look up the course
+    //If it doesn't exist, return 404
+    const course = courses.find(c=>c.id===parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with the given ID was not found');
+
+    //validate
+    //If invalid, return 400 - bad request
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    const result = Joi.ValidationError(req.body, schema); // returns an object
+    console.log(result);
+
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+    //update course
+    //return the updated course
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`))
